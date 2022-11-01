5692365c89401ed14ac450aebcc0abee6ad048c4
MuttiD <dgmutti@hotmail.com>
2022-11-01 22:46:50 +0000

    Add other members of the company to about page

--- run.py
+++ run.py
@@ -1,31 +1,32 @@
 import os
+import json
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-# routing to the index "/"
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# routing to the about page "/about"
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-# routing to the contact page "/contact"
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
-# routing to the careers page "/careers"
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
